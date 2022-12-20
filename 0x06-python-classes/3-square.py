aefee8fb7d4d5cd303bf080906134a6d7c28604a
Wanjiru393 <carolciru393@gmail.com>
2022-12-20 11:47:41 -0800

    class Square that defines a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,3 +15,8 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
+    def area(self):
+        """Def area
+        """
+        return self.__size ** 2
