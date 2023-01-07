3090a79496b68713dcc89b6c95d31226437f594e
Wanjiru393 <carolciru393@gmail.com>
2023-01-07 19:41:23 +0300

    Adding a function that adds 2 integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    returns the sum of a and b
+    """
+    a1, b1 = 0, 0
+    try:
+        a1 = a + 1
+        b1 = b + 1
+    except TypeError:
+        if not a1:
+            return "a must be an integer"
+        if not b1:
+            return "b must be an integer"
+    return (int(a) + int(b))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,12 @@
+# 0x07-python-test_driven_development
+
+Project done during **Full Stack Software Engineering studies** at **ALX**.Aims at learning the basics of coding with **Python**.
+## Technologies
+* Scripts written in Bash 4.3.11(1)
+* Tested on Ubuntu 20.04 LTS
+
+## Files
+All of the following files are scripts and programs written in Python:
+
+| Filename | Description |
+| -------- | ----------- |
